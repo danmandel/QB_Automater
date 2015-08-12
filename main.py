afc5eb62ecee790a54e18f5bccb7ed23b97e5a9a
danmandel <danmandel@users.noreply.github.com>
2015-08-12 14:35:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import win32api
 import win32con
 import SendKeys
 
-
 statement = 'stmt1.txt'
 
-shell = win32com.client.Dispatch("WScript.Shell")
-shell.SendKeys("test")
+def send(text):
+   win32com.client.Dispatch("WScript.Shell").SendKeys(text)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
 def DVA(statement):
     with open(statement) as csvfile:
@@ -21,58 +25,40 @@ def DVA(statement):
 
             return (date, vendor, amount)
         
-##    print ("dates: ", dates)
-##    print "             "
-##    print ("vendors: ", vendors)
-##    print "             "
-##    print ("amounts: ", amounts)
-
-
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-
-
 click(100,120)
 
-
-
-
-def dataentry(date,vendor,amount):
+def dataentry(test):
     #in theory you would first check if you're in the deposits/credits screen
     #but rn i'm going to assume im at deposits.
     #starts off with "deposit to" highlighted
     #abs first thing to do is to select QB screen with a mouseclick
-    shell = win32com.client.Dispatch("WScript.Shell")
-    tab = shell.SendKeys("{TAB}")
+    tab = send("{TAB}")
 
-##    #type in date according to statement row
-##    type something
-##    tab
-##    tab
-##
-##    #type in Received from: vendor
-##    type something
-##    tab
-##
-##    #type in From Account: always income here
-##    type "income"
-##    tab
-##    tab
-##    tab
-##    tab
-##
-##    #type in Amount: amount
-##    type "amount"
-##    enter
-##    enter
-##    
-##    
-##    
-##        
+    #type in date according to statement row
+    send(test)
+    send("{TAB}")
+    send("{TAB}")
+
+    #type in Received from: vendor
+    send(test)
+    send("{TAB}")
+
+    #type in From Account: always income here
+    send("income")
+    send("{TAB}")
+    send("{TAB}")
+    send("{TAB}")
+    send("{TAB}")
+
+    #type in Amount: amount
+    send("amount")
+    send("{ENTER}")
+    send("{ENTER}")
+
+dataentry("test")    
+
+    
+    
+        
 ##for transaction in readCSV:
 ##    dataentry(date,vendor,amount)
-##    
-
-#
