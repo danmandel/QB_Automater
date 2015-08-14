8876d4944342a900cff988ba9d3ac983f27d6cc8
danmandel <danmandel@users.noreply.github.com>
2015-08-13 21:21:51 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -12,31 +12,55 @@ class Checkpoint(object):
     midpoint = (0,0)
     #picture = checkpoint.jpg
     
-def make_checkpoint(name,a_coords,b_coords,c_coords,d_coords,mid_xaxis,mid_yaxis,midpoint):
+def make_checkpoint(name,a_coords,b_coords,c_coords,d_coords):
     checkpoint = Checkpoint()
     checkpoint.name = name
     checkpoint.a_coords = a_coords
     checkpoint.b_coords = b_coords
     checkpoint.c_coords = c_coords
     checkpoint.d_coords = d_coords
-    checkpoint.mid_xaxis = mid_xaxis # eventually make this a formula of ((a[0]+b[0])/2)
-    checkpoint.mid_yaxis = mid_yaxis
-    checkpoint.midpoint = midpoint # eventually make this a formula of mid x/y
+
+    #didn't have to include these in the args
+##    checkpoint.mid_xaxis = mid_xaxis # eventually make this a formula of ((a[0]+b[0])/2)
+##    checkpoint.mid_yaxis = mid_yaxis
+    checkpoint.mid_xaxis = (a_coords[0]+b_coords[0])/2
+    checkpoint.mid_yaxis = (a_coords[1]+d_coords[1])/2
+    checkpoint.midpoint = (checkpoint.mid_xaxis, checkpoint.mid_yaxis)
     return checkpoint
 
 Deposit_To_Textbox = make_checkpoint("Deposit_To",
                              (78,169),(152,169),
-                             (78,186),(152,186),115,178,(115,178))
+                             (78,186),(152,186)) 
+
+Date_Textbox = make_checkpoint("Date",
+                             (214,169),(278,169),
+                             (214,186),(278,186))
+
+Received_From_Textbox = make_checkpoint("Received_From",
+                             (17,254),(142,254),
+                             (17,270),(142,270))
+
+From_Account_Textbox = make_checkpoint("From_Account",
+                             (160,254),(278,254),
+                             (160,269),(278,269))
+
+Amount_Textbox = make_checkpoint("Amount",
+                             (639,254),(759,269),
+                             (639,269),(759,269))
 
 
-
-import win32api
 #x, y = win32api.GetCursorPos()
 
 def move_to(coordinates):
     win32api.SetCursorPos(coordinates)
 
-move_to(Deposit_To_Textbox.midpoint)
+##move_to(Date_Textbox.midpoint)
+##
+##move_to(Received_From_Textbox.midpoint)
+##
+##move_to(From_Account_Textbox.midpoint)
+##
+##move_to(Amount_Textbox.midpoint)
+
 
 #print Deposit_To_Textbox.d_coords
-
