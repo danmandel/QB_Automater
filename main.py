2b0c4a8c193212490ee7adc860cc6970d65c9171
danmandel <danmandel@users.noreply.github.com>
2015-08-13 23:17:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,78 @@
 import csv
-import win32com.client
-import win32api
-import win32con
 import SendKeys
+import win32api
+import win32com.client
+import win32con
+import time
 
-statement = 'stmt1.txt'
+def move_to(coordinates):
+    win32api.SetCursorPos(coordinates)
+    
+def click(coordinates):
+    win32api.SetCursorPos((coordinates))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,coordinates[0],coordinates[1],0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,coordinates[0],coordinates[1],0,0)
 
 def send(text):
-   win32com.client.Dispatch("WScript.Shell").SendKeys(text)
+    win32com.client.Dispatch("WScript.Shell").SendKeys(text)
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
-def dataentry(date,vendor,amount):
-    #in theory you would first check if you're in the deposits/credits screen
-    #but rn i'm going to assume im at deposits.
-    #starts off with "deposit to" highlighted
-    #abs first thing to do is to select QB screen with a mouseclick
+def dataentry(d,v,a):
+    #first thing to add is to select QB screen with a mouseclick
+    #add check if you're in the deposits/credits screen
+    
+    #Date_Textbox
+    click(Date_Textbox.midpoint)
+    click(Date_Textbox.midpoint)
+    click(Date_Textbox.midpoint)
+    send(d)
+    time.sleep(1)
+    
 
-    #type in date according to statement row
-    send("{TAB}")
-    send(date)
-    send("{TAB}")
-    send("{TAB}")
+    #Received_From_Textbox
+    click(Received_From_Textbox.midpoint)
 
-    #type in Received from: vendor
-    send(vendor)
-    send("{TAB}")
+    send(v)
+    time.sleep(1)
 
-    #type in From Account: always income here
+    #From_Account_Textbox
+    click(From_Account_Textbox.midpoint)
     send("income")
-    send("{TAB}")
-    send("{TAB}")
-    send("{TAB}")
-    send("{TAB}")
+    time.sleep(1)
+    
+    #Amount_Textbox
+    click(Amount_Textbox.midpoint)
+    send(a)
+    time.sleep(1)
+    send("{ENTER}")
 
-    #type in Amount: amount
-    send(amount)
-    send("{ENTER}")
-    send("{ENTER}")
+    
+class Checkpoint(object):
+    #square or rectangle ABCD.
+    name = ""   
+    a_coords = (0,0)
+    b_coords = (0,0)
+    c_coords = (0,0)
+    d_coords = (0,0)
+    mid_xaxis = 0
+    mid_yaxis = 0
+    midpoint = (0,0)
+    #picture = checkpoint.jpg
+    
+def make_checkpoint(name,a_coords,b_coords,c_coords,d_coords):
+    checkpoint = Checkpoint()
+    checkpoint.name = name
+    checkpoint.a_coords = a_coords
+    checkpoint.b_coords = b_coords
+    checkpoint.c_coords = c_coords
+    checkpoint.d_coords = d_coords
+
+    #didn't have to include these in the args
+    checkpoint.mid_xaxis = (a_coords[0]+b_coords[0])/2
+    checkpoint.mid_yaxis = (a_coords[1]+d_coords[1])/2
+    checkpoint.midpoint = (checkpoint.mid_xaxis, checkpoint.mid_yaxis)
+    return checkpoint
+
 
 def DVA(statement):
     with open(statement) as csvfile:
@@ -49,14 +81,40 @@ def DVA(statement):
         for transaction in readCSV:
             date = transaction[0]
             vendor = transaction[1]
-            amount = transaction[2]
-            
+            amount = transaction[2]        
+
             dataentry(date,vendor,amount)
+            print(date,vendor,amount)
+            print "  "
+            
+def get_cursor_location():
+    x, y = win32api.GetCursorPos()
+    print x,y
 
-           
-        
-click(100,120)
+    
+statement = "stmt2.txt"    
 
+Deposit_To_Textbox = make_checkpoint("Deposit_To",
+                             (78,169),(152,169),
+                             (78,186),(152,186)) 
+
+Date_Textbox = make_checkpoint("Date",
+                             (214,169),(278,169),
+                             (214,186),(278,186))
+
+Received_From_Textbox = make_checkpoint("Received_From",
+                             (20,258),(145,259),
+                             (20,270),(145,270))
+
+From_Account_Textbox = make_checkpoint("From_Account",
+                             (160,254),(278,254),
+                             (160,269),(278,269))
+
+Amount_Textbox = make_checkpoint("Amount",
+                             (639,254),(759,269),
+                             (639,269),(759,269))
+
+#get_cursor_location()
 
 
 DVA(statement)    
