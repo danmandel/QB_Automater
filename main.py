147809f3363d442d24a26619c93b37e78cff94d1
danmandel <danmandel@users.noreply.github.com>
2015-09-12 20:38:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,22 @@ import win32con
 import time
 import datetime
 
-##def close_all_windows():
-##    print "Closing all windows at: %s" % time.strftime("%H:%M:%S")
-##    Auto.WinActivate("Yuliya")
-##    Auto.WinActivate(apptitle)
-##    Auto.send("!w")
-##    Auto.send("{DOWN}")
-##    Auto.send("{ENTER}")
-##    print "Closed all windows at: %s" % time.strftime("%H:%M:%S")
-
 def close_all_windows(): # Ends with is_color(50,50,grey == 1) aka blank screen
     print "Calling close_all_windows() at: %s" % time.strftime("%H:%M:%S")
     Auto.WinActivate(apptitle)
     Auto.send("!w")
-    Auto.send("{DOWN}")
-    time.sleep(1)
-    Auto.send("{ENTER 2}")
+    Auto.send("a")
+    if Auto.WinExists("Recording"):
+        time.sleep(2)
+        print "do you want to record this transaction exists"
+        Auto.send("n")      
+    # chooses "close all" dropdown option
     time.sleep(1)
     for x in range(4):
         if is_color(250,250,grey) == 0:
             Auto.send("{ESC}")
             print "Esc attempt %s" % x
-    Auto.Send("{ENTER}")
+    Auto.Send("{ENTER 2}")
     time.sleep(1)
     for x in range(4):
         if is_color(250,250,grey) == 0:
@@ -34,24 +28,43 @@ def close_all_windows(): # Ends with is_color(50,50,grey == 1) aka blank screen
     print "Ended close_all_windows() at: %s" % time.strftime("%H:%M:%S")
     print ("")
 
+def open_make_deposits(bankcode): # Ends with cursor at "Date" textbox.
+    print ("Calling open_make_deposits(bankcode) at: %s" % time.strftime("%H:%M:%S"))
+    Auto.send("!b")
+    Auto.send("d")
+    time.sleep(1)
+    for letter in bankcode[0:3]:
+        Auto.send(letter)
+        time.sleep(1)
+    if is_color(115,177,blue) == 1: # eventually make this check more than one point
+        Auto.send("{TAB}") # Now un-highlighted cursor is in Date
+        time.sleep(1)
+        print "BLUE recognized in drop-down."
+    elif is_color(135,178,blue) == 1: # eventually make this check more than one point
+        Auto.send("{TAB}") # Now un-highlighted cursor is in Date
+        print "ELIF in open_make_deposits() activated for some reason"
+        time.sleep(1)
+        print "BLUE recognized in drop-down. 22222222222222222222"
+    else:
+        print "BLUE NOT RECOGNIZED"
+    print ("Ended open_make_deposits(bankcode) at: %s" % time.strftime("%H:%M:%S"))
+
 def open_home():
     print "Calling open_home() at: %s" % time.strftime("%H:%M:%S")
-    Auto.MouseClick("left", 34, 77)
+    Auto.send("!c")
+    Auto.send("h") # chooses "home" dropdown option
     print "Ended open_home() at: %s" % time.strftime("%H:%M:%S")
 
 def tile_windows():
-    Auto.send("!w") # Tile Windows()
-    time.sleep(1)
-    Auto.send("(DOWN 3)")
-    time.sleep(1)
-    Auto.send("{ENTER}")
-    #ends wherever curlor left off. 
-
-def setup(bankcode): # leaves you with Cursor in Date according to bankcode. 
+    Auto.send("!w")
+    Auto.send("h") # Chooses "home" dropdown option. Ends wherever curlor left off. 
+     
+def setup(bankcode): # Leaves you with cursor in Date textbox according to bankcode. 
     print "Calling setup() at: %s" % time.strftime("%H:%M:%S")
     Auto.WinActivate(apptitle)
     Auto.WinMove(apptitle,"", 0, 0, 1000, 1000)
     close_all_windows()
+    time.sleep(1)
     open_home()
     time.sleep(1)
     Auto.send("{TAB}") # will eventually evolve into select_bank function
@@ -71,7 +84,7 @@ def is_color(x,y,color):
     else:
         return 0
 
-def attempt_send_vendor(v,Type): #starts at
+def attempt_send_vendor(v,Type): # Starts at
     print ("Calling attempt_send_vendor() at: %s" % time.strftime("%H:%M:%S"))
     for letter in v[0:3]:
         Auto.send(letter)
@@ -81,7 +94,7 @@ def attempt_send_vendor(v,Type): #starts at
         time.sleep(1)
         print "Vendor recognized in drop-down."
         if Type == "deposit":
-            Auto.send("{TAB 2}") # Now un-highlighted cursor is in Deposit textbox after 2 more tabs.
+            Auto.send("{TAB }") # Ends with un-highlighted cursor in in "From Account" textbox.
             return 1
         elif Type == "credit":
             pass # Now highlighted cursor is still in Payment textbox.
@@ -94,7 +107,6 @@ def attempt_send_vendor(v,Type): #starts at
         #Highlight failed. Cursor now at end of Account textbox.
         return 0
     print ("Ended attempt_send_vendor() at: %s" % time.strftime("%H:%M:%S"))
-
         
 def attempt_send_amount(a,Type):       
     Auto.send(a) #Amount
@@ -129,31 +141,20 @@ def attempt_send_account(Type):
         print "Function 'attempt_send_account' failed."
         return 0
 
-
-def attempt_send_date(d):
+def attempt_send_date(d): # Ends with cursor in "Received From" textbox.
     print ("Called attempt_send_date(d) at: %s" % time.strftime("%H:%M:%S"))
     time.sleep(1)
     Auto.send(d)
     Auto.send("{TAB 2}")
     print ("Ended attempt_send_date() at: %s" % time.strftime("%H:%M:%S"))
     print ("")
-    time.sleep(3)
-    
-
-##add transaction Id
-##    make Transaction a class
-
-##class Transaction(object):
-##    Transaction.
-##
-##def make_transaction(Date,Name,amount,Type,transaction) #lowercase transaction is the each in for each transaction
-
-    
+    time.sleep(2)
+     
 def DepositEntry(d,v,a,Type,transaction): # starts with Date in deposits highlighted
     #print ("Calling DepositEntry() for transaction number  %s: " % transaction) ######
     print ("Calling DepositEntry() at: %s" % time.strftime("%H:%M:%S"))
     #time.sleep(1)
-    attempt_send_date(d) #Cursor ends up at Account (aka vendor)
+    attempt_send_date(d) # Ends with cursor in "Received From" textbox.
     if attempt_send_vendor(v,Type) == 1: # also executes the function ### how to make it use value without executing?
         print "attempt_send_vendor(v,Type) == 1"
         time.sleep(1)
@@ -174,20 +175,25 @@ def CreditEntry(d,v,a,Type,transaction):
     print "CreditEntry pass on name: %s " % v
 
 def Process(statement):
-    #Auto.WinActivate(apptitle)
+    Auto.WinActivate(apptitle)
     #setup(bankcode)
+    close_all_windows()
     with open(statement) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         counter = 0
         for transaction in readCSV:
-            
-            setup(bankcode) ################### ################## remove this to avoid setup every turn #tile windows
+            #close_all_windows()
+            #setup(bankcode) ################### ################## remove this to avoid setup every turn #tile windows
             date = transaction[0]
             vendor = transaction[1]
             amount = transaction[2]
             
             if float(amount) > 0: # Deposit
                 Type = "deposit"
+                close_all_windows()
+                open_make_deposits(bankcode) # Ends with cursor at "Date" textbox.
+                print "SHOULD be At DATE"
+                time.sleep(1)
                 DepositEntry(date,vendor,amount,Type,transaction)
                 
             elif float(amount) < 0: # Credit
@@ -203,33 +209,22 @@ def Process(statement):
             #time.sleep(1)
             print "Finished Transaction number: %s" % counter
             counter += 1 
-            print ""
             print "______________________________"
             print ""
                 
         print "Processed all transactions at time: "
          
-    
-Skipped_List = []
+            
 
 apptitle = "Yuliya"
-##statement = "stmt2.txt"
-statement = "stmtsampleclean.txt"
+statement = "C:\Python27\Scripts\QB\stmtsampleclean.txt"
 Auto = Dispatch("AutoItX3.Control")
-bankcode = "10030"
+bankcode = "Bank of America Bus"
 current_time = time.strftime("%H:%M:%S")
-black = 0x000000 ###
-grey = 0xABABAB ###
+black = 0x000000 
+grey = 0xABABAB 
+blue = 0x3399FF
+Skipped_List = []
 
-#Auto.WinActivate(apptitle)
 Process(statement)
-
-##Auto.WinActivate("Yuliya")
-##time.sleep(1)
-print Auto.WinExists("Bank")
-
 print "Finished Process"
-
-#print Skipped_List
-
-#print Auto.WinExists("Yuliya")
