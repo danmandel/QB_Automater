5a9602269d092d1e7cbda561f435d7c531f6abe7
danmandel <danmandel@users.noreply.github.com>
2016-04-09 21:45:42 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,43 @@
+conf = {
+    "colors": {
+        "black" : 0x000000,
+        "blackish_grey" : 0x484848,
+        "blue" : 0x3399FF,
+        "green" : 0x4E9E,
+        "grey" : 0xABABAB,
+        "white" : 0xFFFFFF
+        },
+    
+    "transaction_types": {
+        "do_debits" : True,
+        "do_credit" : False
+        },
+
+    "misc": {
+        "apptitle" : "Yuliya",
+        "statement" : "C:\Python27\Scripts\QB\credit_test.txt",
+        "bank_code" : "Bank of America Bus",
+        "request_confirmation" : True,
+        "sleep" : 1,
+        },
+    
+    "methods": {
+        "print_x" : print ("yes this is x")
+        },
+    
+    "messages": {
+        "error_1" : "shits goin down lol",
+        }
+
+    
+}
+
+class Config(object):
+    def __init__(self):
+        self.sleep = 0
+        self.config = conf
+
+    def get_property(self, property_name):
+        if property_name not in self.config.keys():
+            return None # Avoids KeyError.
+        return self.config[property_name]
